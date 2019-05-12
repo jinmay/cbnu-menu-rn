f446cbf788db5d68745b7880376dc86e3ccff66e
jinmay <britzegs@gmail.com>
2019-05-12 14:21:43 +0900

    Menu 스크린을 Container / Presenter 구조로 변경

---
+++ screens/Menu/MenuContainer.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import MenuPresenter from './MenuPresenter';
+
+export default class MenuContainer extends Component {
+  render() {
+    return <MenuPresenter />
+  }
+}
