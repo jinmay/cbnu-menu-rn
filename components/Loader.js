27852d83f238563b687b996f66577f918ace32b3
jinmay <britzegs@gmail.com>
2019-05-13 14:29:12 +0900

    Loader 컴포넌트 배경색 변경

--- components/Loader.js
+++ components/Loader.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import styled from 'styled-components';
-import { ActivityIndicator } from 'react-native';
+import React from "react";
+import styled from "styled-components";
+import { ActivityIndicator } from "react-native";
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
+  background-color: black;
 `;
 
 export default () => (
   <Container>
     <ActivityIndicator />
   </Container>
-)
+);
